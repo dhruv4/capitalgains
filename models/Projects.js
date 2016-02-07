e7140cf8a6c9630368c282423e235bef2e9b068a
Rohan Pandit <pandit.rohan@gmail.com>
2016-02-07 00:34:31 -0500

    added User and Project schema to db

---
+++ models/Projects.js
@@ -0,0 +1,17 @@
+var bcrypt = require('bcrypt-nodejs');
+var crypto = require('crypto');
+var mongoose = require('mongoose');
+
+var projectSchema = new mongoose.Schema({
+
+		title: String,
+		borrower: String,
+		description: String,
+		status: Boolean,
+		money: Number,
+		date: Date,
+		payments: [ {date: Date, amount: Number}],
+		remaining: Number
+	});
+
+module.exports = mongoose.model('Project', projectSchema);
